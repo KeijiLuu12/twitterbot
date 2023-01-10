c49bff3070f11ce37954a10794718ce9b1edf552
KeijiLuu12 <117475984+KeijiLuu12@users.noreply.github.com>
2023-01-09 21:15:50 -0500

    Update reply.py

--- reply.py
+++ reply.py
@@ -13,13 +13,24 @@ def tweet_reply(tweet_api: tweepy.API, message: str, user_id: int):
 
     print("Had an interaction today!")
 
-
 if __name__ == '__main__':
+
+    with open('/Users/kiwi/coding/PythonCode/twitterbot/mentionedid.txt', 'r', encoding='utf-8') as mentioned_file:
+        last_mentioned = mentioned_file.read()
     api = api()
-    mentions = api.mentions_timeline()
+
+    if last_mentioned == '':
+        mentions = api.mentions_timeline(count = 2)
+    else:
+        mentions = api.mentions_timeline(count=3, since_id=last_mentioned)
+    for mention in mentions:
+        with open('/Users/kiwi/coding/PythonCode/twitterbot/mentionedid.txt', 'w', encoding='utf-8') as mentioned_file:
+            mentioned_file.write(str(mentions[0].id)) 
+            
     if mentions:
         user_id_response = mentions[0].id
         user_name = mentions[0].user.screen_name
         reply = "Wow! You have broken me out of the vicious cycle of a bot!\nNow back to the grind..."
         twitter_reply = '@' + user_name + '\n' + reply
         tweet_reply(api, twitter_reply, user_id_response)
+ 
